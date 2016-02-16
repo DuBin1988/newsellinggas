dc1521e9b971d57857fe47bedf126c2b64f71ad9
LiYaFeng1993 <550302825@qq.com>
2016-02-17 01:22:06 +0800

    建档时选择抄表员读抄表员电话 和抄表员录入页面

---
+++ s2/s2/Pages/抄表员录入.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace s2
+{
+    public partial class 抄表员录入 : UserControl
+    {
+        public 抄表员录入()
+        {
+            // Required to initialize variables
+            InitializeComponent();
+        }
+    }
+}
