c65a8f0cd0834cb247c80bf6e1ed0ba342fbc8c2
Administrator <hikki114@163.com>
2016-03-15 14:33:27 +0800

    添加短信模板发送服务

--- src/main/java/com/aote/rs/sms/SmsService.java
+++ src/main/java/com/aote/rs/sms/SmsService.java
@@ -1,5 +1,6 @@
 package com.aote.rs.sms;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
@@ -17,6 +18,26 @@ import com.aote.rs.util.BeanUtil;
 @Path("/sms")
 @Scope("prototype")
 public class SmsService {
+
+	@Path("/send/{phone}/{templatename}")
+	@POST
+	public JSONObject sendTemplate(String param,
+			@QueryParam("phone") String phone,
+			@QueryParam("templatename") String template) {
+		JSONObject result = new JSONObject();
+		try {
+			//获得模板内容
+			String msg ="";
+			//msg = getSmsTemplateByName(template);
+			// 获得配置的短信实现类
+			ISms sms = (ISms) BeanUtil.getBean(ISms.class);
+			JSONObject attr = new JSONObject();
+			result = sms.sendsms(phone, msg, attr);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 	@Path("/send/{phone}/{msg}")
 	public JSONObject sendmsg(@QueryParam("phone") String phone,
