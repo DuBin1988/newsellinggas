f7212b0201a4c308d28b2b6a26ce74a20e5519aa
Administrator <hikki114@163.com>
2016-03-15 17:16:10 +0800

    短信模板管理

--- src/main/java/com/aote/rs/sms/SmsService.java
+++ src/main/java/com/aote/rs/sms/SmsService.java
@@ -1,12 +1,20 @@
 package com.aote.rs.sms;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
+import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 
+import com.aote.rs.tcp.TcpService;
 import com.aote.rs.util.BeanUtil;
 
 /**
@@ -19,6 +27,11 @@ import com.aote.rs.util.BeanUtil;
 @Scope("prototype")
 public class SmsService {
 
+	static Logger log = Logger.getLogger(SmsService.class);
+
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
 	@Path("/send/{phone}/{templatename}")
 	@POST
 	public JSONObject sendTemplate(String param,
@@ -26,9 +39,16 @@ public class SmsService {
 			@QueryParam("templatename") String template) {
 		JSONObject result = new JSONObject();
 		try {
-			//获得模板内容
-			String msg ="";
-			//msg = getSmsTemplateByName(template);
+			// 获得模板内容
+			String msg = "";
+			msg = getSmsTemplateByName(template);
+			// 替换参数
+			JSONObject p = new JSONObject(param);
+			Iterator iter = p.keys();
+			while(iter.hasNext()){
+				String key = (String)iter.next();
+				msg=msg.replace("#"+key+"#", p.getString(key));
+			}
 			// 获得配置的短信实现类
 			ISms sms = (ISms) BeanUtil.getBean(ISms.class);
 			JSONObject attr = new JSONObject();
@@ -36,6 +56,20 @@ public class SmsService {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return result;
+	}
+
+	/**
+	 * 根据短信模板名获得内容
+	 * @param name
+	 * @return
+	 */
+	private String getSmsTemplateByName(String name) {
+		String result = "";
+		List list = this.hibernateTemplate
+				.find("from t_smstemplate where name ='" + name + "'");
+		Map<String, Object> map = (Map<String, Object>) list.get(0);
+		result = map.get("f_content").toString();
 		return result;
 	}
 
