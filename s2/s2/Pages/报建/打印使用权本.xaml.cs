42e06d38a829567ee08dbb6c36088481c39783d1
szm <szm@DESKTOP-UKF769E>
2016-03-16 14:34:54 +0800

    散户流程

---
+++ s2/s2/Pages/报建/打印使用权本.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Com.Aote.Pages
+{
+	public partial class 打印使用权本 : UserControl
+	{
+        public 打印使用权本()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+       
+	}
+}
