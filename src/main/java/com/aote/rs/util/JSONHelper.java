8ac6a35a641ded028f682d6358343a958a343fe3
Administrator <hikki114@163.com>
2016-03-17 11:13:54 +0800

    jsonToMap方法

--- src/main/java/com/aote/rs/util/JSONHelper.java
+++ src/main/java/com/aote/rs/util/JSONHelper.java
@@ -29,6 +29,29 @@ import com.aote.expression.ExpressionGenerator;
 public class JSONHelper {
 	static Logger log = Logger.getLogger(JSONHelper.class);
 
+	
+	/**
+	 * json对象转map
+	 * @param jsonObject
+	 * @return
+	 */
+	public static HashMap<String, Object> toHashMap(JSONObject jsonObject) {
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		Iterator it = jsonObject.keys();
+		// 遍历jsonObject数据，添加到Map对象
+		while (it.hasNext()) {
+			String key = String.valueOf(it.next());
+			Object value;
+			try {
+				value = jsonObject.get(key);
+			} catch (JSONException e) {
+				throw new RuntimeException(e);
+			}
+			result.put(key, value);
+		}
+		return result;
+	}
+	
 	/**
 	 * json对象转map
 	 * 
