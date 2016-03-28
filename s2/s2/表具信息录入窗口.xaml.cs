5053675343f97422b5cd5712193ecb19957e7d5e
Administrator <Administrator@USER-20160109EA>
2016-03-28 18:10:02 +0800

    （用户建档，表具信息录入窗口，设备信息录入窗口）后面那点报错就是刚加的那个对象

---
+++ s2/s2/表具信息录入窗口.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace s2
+{
+    public partial class 表具信息录入窗口 : ChildWindow
+    {
+        public 表具信息录入窗口()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
+
