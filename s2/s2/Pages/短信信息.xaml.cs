adae382eb9f2f9b0b9112e0aa7fd63af0a5cb54f
HelloWorld <HelloWorld@Ambiguous>
2016-04-05 11:01:40 +0800

    短息那功能和 部分分公司

---
+++ s2/s2/Pages/短信信息.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Com.Aote.Behaviors;
+using Com.Aote.ObjectTools;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Com.Aote.Pages
+{
+    public partial class 短信信息 : UserControl
+    {
+        public 短信信息()
+        {
+            // Required to initialize variables
+            InitializeComponent();
+        }
+
+        private void oneSend_Click(object sender, RoutedEventArgs e)
+        {
+            BaseObjectList list = daninfos.ItemsSource as BaseObjectList;
+
+
+            //对于每一条记录
+            foreach (GeneralObject go in list)
+            {
+                if (go == daninfos.SelectedItem)
+                {
+                    // id
+                    String smsId = go.GetPropertyValue("id").ToString();
+
+                    String sql = "update t_sms set f_state='未发' where id=" + smsId + "";
+                    HQLAction action = new HQLAction();
+                    action.Name = "abc";
+                    action.HQL = sql;
+                    action.WebClientInfo = Application.Current.Resources["dbclient"] as WebClientInfo;
+                    action.Invoke();
+                }
+            }
+        }
+
+        private void allSend_Click(object sender, RoutedEventArgs e)
+        {
+            BaseObjectList list = daninfos.ItemsSource as BaseObjectList;
+
+
+            //对于每一条记录
+            foreach (GeneralObject go in list)
+            {
+                // id
+                String smsId = go.GetPropertyValue("id").ToString();
+
+                String sql = "update t_sms set f_state='未发' where id="+ smsId +"";
+                HQLAction action = new HQLAction();
+                action.Name = "abc";
+                action.HQL = sql;
+                action.WebClientInfo = Application.Current.Resources["dbclient"] as WebClientInfo;
+                action.Invoke();
+            }
+        }
+    }
+}
