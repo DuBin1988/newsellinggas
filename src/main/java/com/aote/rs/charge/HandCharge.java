8ccf614bc2f4187f068daa829f14803f666fd47c
bianzk <61577962@>
2016-04-14 02:49:31 +0800

    捷宝A188抄表机DBF文件操作

--- src/main/java/com/aote/rs/charge/HandCharge.java
+++ src/main/java/com/aote/rs/charge/HandCharge.java
@@ -59,8 +59,9 @@ public class HandCharge {
 
 	@Autowired
 	private HibernateTemplate hibernateTemplate;
-
-	// private int stairmonths;
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
 
 	private String stardate;
 	private String enddate;
@@ -93,7 +94,7 @@ public class HandCharge {
 	public JSONArray ReadRecordInput(@PathParam("operator") String operator) {
 		JSONArray array = new JSONArray();
 		List<Object> list = this.hibernateTemplate.find(
-				"from t_handplan where f_inputtor=? and f_state='未抄表'",
+				"from t_handplan where f_userid is not null and f_inputtor=? and f_state='未抄表'",
 				operator);
 		for (Object obj : list) {
 			// 把单个map转换成JSON对象
