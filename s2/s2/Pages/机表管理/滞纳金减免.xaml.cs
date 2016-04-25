8b340d027584d8049740a5e81014a379e1b9e9bb
Administrator <hikki114@163.com>
2016-04-25 18:01:03 +0800

    滞纳金减免

---
+++ s2/s2/Pages/机表管理/滞纳金减免.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Com.Aote.ObjectTools;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Linq;
+using Com.Aote.Utils;
+using Com.Aote.Controls;
+
+namespace Com.Aote.Pages
+{
+	public partial class 滞纳金减免 : UserControl
+	{
+        public 滞纳金减免()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+         
+      
+
+    }
+}
