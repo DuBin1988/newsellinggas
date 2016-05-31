725e35f7707bbd157be2e824adb3191c2c553155
Administrator <hikki114@163.com>
2016-05-31 12:40:34 +0800

    回收率报表，收据管理

---
+++ s2/s2/Pages/收据管理/收据分配.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Com.Aote.Pages
+{
+    public partial class 收据分配 : UserControl
+    {
+        public 收据分配()
+        {
+            InitializeComponent();
+        }
+    }
+}
